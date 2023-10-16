c51b04f86040704c3aa4fa6c3a4f03d7c0a50a3d
samirailfans911 <saminahdi53@gmail.com>
2023-10-16 15:32:07 +0700

    menambahkan validasi data dengan form validasi pada controller matakuliah

---
+++ application/views/view-data-matakuliah.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <title>Tampilan Data Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan='3'>
+                    Tampilan Data Matakuliah
+                </th>
+            </tr>
+            <tr>
+                <th>Kode MTK</th>
+                <td>:</td>
+                <td>
+                    <?= $kode; ?>
+                </td>
+            </tr>
+            <tr>
+                <th> Nama MTK</th>
+                <td>:</td>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>SKS</th>
+                <td>:</td>
+                <td>
+                    <?= $sks; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan='3' align='center'>
+                    <a href='<?= base_url('matakuliah'); ?>'>Kembali</a>
+                </td>
+            </tr>
+        </table>
+</center>
+</body>
+
+</html>
