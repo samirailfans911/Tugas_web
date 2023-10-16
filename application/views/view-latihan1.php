dca55174a2fedde7c7291125a8e188340cdb1026
samirailfans911 <saminahdi53@gmail.com>
2023-10-16 15:49:00 +0700

    menambahkan validasi data dengan form validasi pada controller matakuliah

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -1,12 +1,14 @@
-<html>
+<html lang="en">
 <head>
- <title>Latihan 1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <title>Latihan 1</title>
 </head>
 <body>
- Halo Kawan.. Yuk kita belajar web programming..!!!<br>
- Nilai 1 = <?= $nilai1; ?>
- Nilai 2 = <?= $nilai2; ?>
- ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 </html>
