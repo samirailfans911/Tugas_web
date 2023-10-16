e685e0dafc45ae6f6bf2da8e9a6bebebbe8c9c30
samirailfans911 <saminahdi53@gmail.com>
2023-10-16 15:39:20 +0700

    menambahkan validasi data dengan form validasi pada controller matakuliah

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1><h4>Nama</h4>
+    <ul type="disc">
+    <li>Nama Depan : Muhammad</li>
+    <li>Nama Belakang : Sami</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+    <li> Majlis Ta'lim Azamzami</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+    <li>Sepeda</li>
+    <li>Renang</li>
+    </ul>
+</section>
